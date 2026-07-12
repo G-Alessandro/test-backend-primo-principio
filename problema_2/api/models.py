b77805b296a34905db9d183c6a3f75b4e9c5c5a7
Alessandro <galloalessandro77@gmail.com>
2026-07-12 17:16:33 +0200

    Aggiunto modello StoricoEventi

--- problema_2/api/models.py
+++ problema_2/api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class StoricoEventi(models.Model):
+    doy = models.IntegerField()
+    event_index = models.IntegerField()
+    x_value = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["doy", "event_index"],
+                name="vincolo_eventi"
+            )
+        ]
